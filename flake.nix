8aea278042f2a2557fb766feabc99c0f4a6d8cbc
arud <dominique.arnou@cnieg.fr>
2026-05-19 22:23:13 +0200

    fix: reorg flake syntax

--- flake.nix
+++ flake.nix
@@ -1,43 +1,60 @@
-outputs = { self, nixpkgs, disko, securix, home-manager, ... }@inputs: {
-  nixosConfigurations = {
+{
+  description = "Configuration NixOS sécurisée et moderne pour Laptop";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     
-    # 1. Cible pour votre VRAI laptop
-    nix-laptop = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        disko.nixosModules.disko
-        securix.nixosModules.default
-        home-manager.nixosModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.nixos = import ./home/home.nix;
-        }
-        ./disko-config.nix
-        ./configuration.nix
-        ./security.nix
-        { hardware.enableRedistributableFirmware = true; } 
-      ];
-    };
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
 
-    # 2. Cible dédiée aux tests QEMU
-    nix-vm = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        disko.nixosModules.disko
-        securix.nixosModules.default
-        home-manager.nixosModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.nixos = import ./home/home.nix;
-        }
-        ./disko-config.nix
-        ./configuration.nix
-        ./security.nix
-        ({ modulesPath, ... }: {
-          imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
-        })
-      ];
-    };
+    securix.url = "github:cloud-gouv/securix";
+    securix.inputs.nixpkgs.follows = "nixpkgs";
 
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
-};
+
+  outputs = { self, nixpkgs, disko, securix, home-manager, ... }@inputs: {
+    nixosConfigurations = {
+      
+      # 1. Cible pour votre VRAI laptop
+      nix-laptop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          disko.nixosModules.disko
+          securix.nixosModules.default
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.nixos = import ./home/home.nix;
+          }
+          ./disko-config.nix
+          ./configuration.nix
+          ./security.nix
+          { hardware.enableRedistributableFirmware = true; } 
+        ];
+      };
+
+      # 2. Cible dédiée aux tests QEMU
+      nix-vm = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          disko.nixosModules.disko
+          securix.nixosModules.default
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.nixos = import ./home/home.nix;
+          }
+          ./disko-config.nix
+          ./configuration.nix
+          ./security.nix
+          ({ modulesPath, ... }: {
+            imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+          })
+        ];
+      };
+
+    }; # Fin de nixosConfigurations
+  };   # Fin de outputs
+}
